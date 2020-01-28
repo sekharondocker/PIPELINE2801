f47e600113710eee6b8ec87a0f581b348458adb0
sekharondocker <60387773+sekharondocker@users.noreply.github.com>
2020-01-28 16:45:57 +0530

    Create Sample.sh

---
+++ Sample.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Change mad at `date`"
+if [ $# -gt 0 ]
+then
+if [ -f $1 ]
+then
+  echo "Contents of the file..."
+  cat $1
+else
+  echo "File Not Found.."$1
+fi
+else
+  echo "Not enough command line arguments..."
+fi
